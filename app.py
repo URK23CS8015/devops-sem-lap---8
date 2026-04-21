c332d14c1b8ef86a2a4815b1f041dfa47edf7a1e
URK23CS8015 <afrinbanu@karunya.edu.in>
2026-04-21 12:01:18 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+tasks = []
+
+@app.route('/')
+def home():
+    return "<h1>To-Do</h1>" + str(tasks) + \
+           "<form action='/add' method='post'>" \
+           "<input name='task'><button>Add</button></form>"
+
+@app.route('/add', methods=['POST'])
+def add():
+    tasks.append(request.form['task'])
+    return "Added <a href='/'>Back</a>"
+
+
+
+app.run(host="0.0.0.0",port=5000)
+
